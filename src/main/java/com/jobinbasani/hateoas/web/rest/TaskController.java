0e63935bd7245c2bc7817c29d4389434ac1bab16
Jobin Basani <jobinbasani@gmail.com>
2020-04-07 16:21:22 -0300

    Cleanup

--- src/main/java/com/jobinbasani/hateoas/web/rest/TaskController.java
+++ src/main/java/com/jobinbasani/hateoas/web/rest/TaskController.java
@@ -8,16 +8,14 @@ import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.RepresentationModel;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api")
 public class TaskController {
     private final TaskService taskService;
     private final TaskAssembler taskAssembler;
